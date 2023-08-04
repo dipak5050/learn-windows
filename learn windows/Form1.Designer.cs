6d8cb804ae1a806bfe93b71d7b77b851ec205de6
dipak5050 <dipakbapodra0111@gmail.com>
2023-08-04 17:48:09 +0530

    Add project files.

---
+++ learn windows/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace learn_windows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txtans = new System.Windows.Forms.TextBox();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(25, 12);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 20);
+            this.txt1.TabIndex = 0;
+            this.txt1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(2, 15);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(17, 13);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "A:";
+            this.lbl1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(2, 62);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(16, 13);
+            this.lbl2.TabIndex = 3;
+            this.lbl2.Text = "b:";
+            this.lbl2.Click += new System.EventHandler(this.lbl2_Click);
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(25, 59);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 20);
+            this.txt2.TabIndex = 2;
+            this.txt2.TextChanged += new System.EventHandler(this.txt2_TextChanged);
+            // 
+            // txtans
+            // 
+            this.txtans.Location = new System.Drawing.Point(25, 149);
+            this.txtans.Name = "txtans";
+            this.txtans.Size = new System.Drawing.Size(100, 20);
+            this.txtans.TabIndex = 4;
+            this.txtans.TextChanged += new System.EventHandler(this.txtans_TextChanged);
+            // 
+            // btnadd
+            // 
+            this.btnadd.Location = new System.Drawing.Point(25, 120);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(75, 23);
+            this.btnadd.TabIndex = 5;
+            this.btnadd.Text = "add";
+            this.btnadd.UseVisualStyleBackColor = true;
+            this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "b:";
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(25, 85);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(100, 20);
+            this.txt3.TabIndex = 6;
+            this.txt3.TextChanged += new System.EventHandler(this.txt3_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.txtans);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.txt1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txtans;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt3;
+    }
+}
+
---
+++ learn windows/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace learn_windows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+            int a, b, c,d;
+            a = Convert.ToInt32(txt1.Text);
+            b = Convert.ToInt32(txt2.Text);
+            d = Convert.ToInt32(txt3.Text);
+            //c = a + b;
+         //   txtans.Text = c.ToString();
+            if(a>=b)
+            {
+                txtans.Text = a.ToString();
+            }
+            else if(b>=d)
+            {
+                txtans.Text = b.ToString();
+            }
+            else 
+            {
+                txtans.Text = d.ToString();
+            }
+        }
+
+        private void txtans_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
